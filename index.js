0ab50be41e50fbb597f4d33ccaf27f535bf0c574
Aniket Kumar <aniketkumar@LK-LP-1130.local>
2024-01-16 23:42:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+//Server production assest
+//if(process.env.NODE_ENV === "production"){
+//  app.use(express.static(path.join("public/build")))
+//  app.get("*", (req,res) => {
+//    res.sendFile(path.resolve(__dirname, "public", "build", "index.html"))
+//  });
+//}
+const PORT = process.env.PORT || 5001
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("public/build"))
+}
+const server = app.listen(PORT, () =>
+  console.log(`Server started on ${PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
