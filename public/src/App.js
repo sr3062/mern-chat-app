4388a97cce801759aeb04a87dc3a817d22898e07
Aniket Kumar <aniket.kumar@lendingkart.com>
2024-01-18 23:56:59 +0530

    changes 4

--- public/src/App.js
+++ public/src/App.js
@@ -4,6 +4,7 @@ import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./utils/ProtectedRoutes"
 export default function App() {
   return (
     <BrowserRouter>
@@ -11,7 +12,11 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/setAvatar" element={<SetAvatar />} />
-        <Route path="/" element={<Chat />} />
+        <Route path="/" element={
+        <ProtectedRoute>
+        <Chat/>
+        </ProtectedRoute>
+     }/>
       </Routes>
     </BrowserRouter>
   );
